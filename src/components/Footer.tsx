cf5c21cb55ebe9c5a26ada6983b32fe89eefdd55
yuanzhe-jia <yuanzhe.dev@gmail.com>
2026-06-28 22:14:16 +0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-caramel text-white py-6 mt-16 relative z-10">
       <div className="max-w-7xl mx-auto px-6 text-center">
-        <p className="font-sans">© 2026 Bubble Bliss | Sparkling Wines | Wine Articals </p>
+        <p className="font-sans">© Bubble Bliss | Sparkling Wines & Cocktails | Wine Recipes & Articals </p>
       </div>
     </footer>
   );
