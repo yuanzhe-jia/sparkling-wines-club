0ea75c514a0721add47582da2451a6fafde61b87
yuanzhe-jia <yuanzhe.dev@gmail.com>
2026-06-29 13:42:59 +0800

    add vercel speed insights monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App';
 import './index.css';
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <HashRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </HashRouter>
   </React.StrictMode>,
 );
